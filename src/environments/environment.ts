a9064006f2ab7cff07d2a4dc709685e64096e63b
teknoloje <teknoloje@teknoloje.com>
2026-06-09 15:47:14 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   // apiUrl: 'http://13.58.156.144/api/'
   // apiUrl: 'https://localhost:7208/api'
-  apiUrl: 'https://localhost:44367/api',
+  apiUrl: 'http://10.0.0.1:8102/api/',  
+  // apiUrl: 'https://localhost:44367/api',
   //apiUrl: '/api',
   //apiUrl: ' https://tek.teknoloje.com/api',
 
